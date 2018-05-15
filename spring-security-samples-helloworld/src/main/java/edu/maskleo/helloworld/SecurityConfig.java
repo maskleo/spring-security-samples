80530b7c992cfc42895d17c2ff0477bfde76dcc1
maskleo <masque@aliyun.com>
2018-05-15 16:39:24 +0800

    添加用户权限

--- spring-security-samples-helloworld/src/main/java/edu/maskleo/helloworld/SecurityConfig.java
+++ spring-security-samples-helloworld/src/main/java/edu/maskleo/helloworld/SecurityConfig.java
@@ -1,12 +1,17 @@
 package edu.maskleo.helloworld;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
-    
+
     /**
      * 自定义配置
      */
@@ -15,10 +20,24 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers("/css/**", "/js/**", "/fonts/**", "/index").permitAll()  // 虽都可以访问
-                .antMatchers("/users/**").hasRole("USER")   // 需要相应的角色才能访问
-                .antMatchers("/admins/**").hasRole("ADMIN")   // 需要相应的角色才能访问
+                .antMatchers("/user/**").hasRole("USER")   // 需要相应的角色才能访问
+                .antMatchers("/admin/**").hasRole("ADMIN")   // 需要相应的角色才能访问
                 .and()
                 .formLogin()   //基于 Form 表单登录验证
                 .loginPage("/login").failureUrl("/login-error");  // 自定义登录界面
     }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService());
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager(); // 在内存中存放用户信息
+        manager.createUser(User.withUsername("user").password("123456").roles("USER").build());
+        manager.createUser(User.withUsername("admin").password("123456").roles("USER", "ADMIN").build());
+        return manager;
+    }
+
 }
